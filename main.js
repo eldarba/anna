dc2ddba1a0130a0f8c015d4eea2ba3ea329a6aa9
Eldar <eldarba@gmail.com>
2025-03-19 11:48:48 +0200

    add images and arrows

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 "use strict";
 {
-    const coppingMechanismsSelectSpan = document.getElementById("coppingMechanismsSelectSpan");
+    // constant elements
+    const coppingMechanismsSelect = document.getElementById("coppingMechanismsSelect");
     const btAddMechanism = document.getElementById("btAddMechanism");
     const newMechanismBox = document.getElementById("newMechanismBox");
+    const removeMechanismBt = document.getElementById("removeMechanismBt");
 
+    // constants
     const coppingMechanisms = [
+        "-- Choose Copping Mechanism --",
         "The Pleaser",
         "The Smoker",
         "The Protestor",
@@ -14,10 +18,11 @@
 
     function loadMechanisms() {
         let html = "";
-        for (const x of coppingMechanisms) {
-            html += `<option value="">${x}</option>`;
+        for (let i = 0; i < coppingMechanisms.length; i++) {
+            const x = coppingMechanisms[i];
+            html += `<option id="${i}">${x}</option>`;
         }
-        coppingMechanismsSelectSpan.innerHTML = html;
+        coppingMechanismsSelect.innerHTML = html;
     }
 
     btAddMechanism.addEventListener("click", function () {
@@ -27,5 +32,15 @@
         }
         coppingMechanisms.push(newMechanismBox.value);
         loadMechanisms();
+        newMechanismBox.value = "";
+        coppingMechanismsSelect.selectedIndex = coppingMechanisms.length-1;
+    });
+
+    removeMechanismBt.addEventListener("click", function () {
+        const selectedIndex = coppingMechanismsSelect.selectedIndex;
+        if (selectedIndex > 0) {
+            alert("Remove this copping mechanism?");
+            coppingMechanismsSelect.remove(selectedIndex);
+        }
     });
 }
